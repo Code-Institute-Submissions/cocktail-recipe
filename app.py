e537795c996cef6a7692cb800002c44d22f38932
Simon_Mac_Aree <smacr25@gmail.com>
2018-08-27 15:30:35 +0000

    adding a base html

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo 
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'cookbook-data'
+app.config["MONGO_URI"] = 'mongodb://cocktail-root:Forzaaudia1@ds125352.mlab.com:25352/cookbook-data'
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello_test():
-    return "Hello World test"
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", 
+    recipes=mongo.db.recipes.find())
+
+# @app.route('/')
+# def hello_test():
+#     return "Hello World test"
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
