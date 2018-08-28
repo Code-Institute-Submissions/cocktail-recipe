4ba54aa800abdfa46b9359cf9b4698398cf9c879
Simon_Mac_Aree <smacr25@gmail.com>
2018-08-28 10:57:55 +0000

    adding add recipe html and form

--- app.py
+++ app.py
@@ -16,9 +16,16 @@ def get_recipes():
     return render_template("recipes.html", 
     recipes=mongo.db.recipes.find())
 
-# @app.route('/')
-# def hello_test():
-#     return "Hello World test"
+@app.route('/cocktail_image')
+def cocktail_image():
+    return render_template("recipes.html",
+    create=mongo.db.create.find())
+    
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template('addrecipe.html')
+
+
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
