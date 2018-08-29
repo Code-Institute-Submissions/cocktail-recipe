a1f1730b623987d628e3e83632d1944b2fcf5b37
Simon_Mac_Aree <smacr25@gmail.com>
2018-08-29 08:39:05 +0000

    first commit to github. eailur commits where sent to heroku

--- app.py
+++ app.py
@@ -13,20 +13,29 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
+    recipes=mongo.db.recipes.find()
+    
+    #print("recipes", len(list(recipes)))
     return render_template("recipes.html", 
-    recipes=mongo.db.recipes.find())
+    recipes=recipes)
 
 @app.route('/cocktail_image')
 def cocktail_image():
     return render_template("recipes.html",
-    create=mongo.db.create.find())
+    images=mongo.db.images.find())
     
 @app.route('/add_recipe')
 def add_recipe():
-    return render_template('addrecipe.html')
+    return render_template('addrecipe.html',
+    difficulty=mongo.db.difficulty.find())
+
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    recipes = mongo.db.recipes
+    recipes.insert_one(request.form.to_dict())
+    return redirect(url_for('get_recipes'))
 
 
-    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
         port=int(os.environ.get('PORT')),
