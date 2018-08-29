401a5d8c2d99f8ef9e79e88505dbbab04ce9f53c
Simon_Mac_Aree <smacr25@gmail.com>
2018-08-29 17:38:09 +0000

    adding images

--- app.py
+++ app.py
@@ -45,6 +45,11 @@ def update_recipes(recipe_id):
     })
     return redirect(url_for('get_recipes'))
 
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipes(recipe_id):
+    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
+    return redirect(url_for('get_recipes'))
+
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
